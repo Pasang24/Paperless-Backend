a1b3065b204b16c3caad27069d2047fea14363fa
Pasang24 <pasangtheeng14@gmail.com>
2025-05-18 22:31:08 +0545

    chore: setup drizzle orm and create schema for user table

---
+++ src/db/schema/user.ts
@@ -0,0 +1,24 @@
+import {
+  pgTable,
+  varchar,
+  uuid,
+  pgEnum,
+  timestamp,
+  unique,
+} from "drizzle-orm/pg-core";
+
+export const providerEnum = pgEnum("provider", ["email", "google", "github"]);
+
+export const user = pgTable(
+  "user",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: varchar("name", { length: 255 }).notNull(),
+    email: varchar("email", { length: 255 }).notNull(),
+    password: varchar("password", { length: 255 }).notNull(),
+    provider: providerEnum("provider").default("email"),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at"),
+  },
+  (table) => [unique().on(table.email, table.provider)]
+);
