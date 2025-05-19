e9626489d85b40460b2e2038ba894ff029146fbf
Pasang24 <pasangtheeng14@gmail.com>
2025-05-19 21:00:18 +0545

    chore: implement cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express from "express";
+import cors, { CorsOptions } from "cors";
+import { env } from "./config/env";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.route";
 
 const app = express();
 
+const corsOptions: CorsOptions = {
+  origin: env.FRONTEND_URL,
+  methods: ["GET", "POST", "UPDATE", "PUT", "PATCH", "OPTIONS"],
+  credentials: true,
+};
+
 //middlewares
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
